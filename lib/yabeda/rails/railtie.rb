6d6328396816d12be61f2f96a54ea4f7920f11ff
Richard Dowden <rd@richarddowden.com>
2019-01-15 14:12:43 +0000

    Configure Yabeda when starting app via Puma CLI

--- lib/yabeda/rails/railtie.rb
+++ lib/yabeda/rails/railtie.rb
@@ -4,9 +4,17 @@ module Yabeda
   module Rails
     class Railtie < ::Rails::Railtie # :nodoc:
       config.after_initialize do
-        next unless ::Rails.const_defined?(:Server)
+        next unless rails_server? || puma_server?
 
         ::Yabeda::Rails.install!
+      end
+
+      def rails_server?
+        ::Rails.const_defined?(:Server)
+      end
+
+      def puma_server?
+        ::Rails.const_defined?('Puma::CLI')
       end
     end
   end

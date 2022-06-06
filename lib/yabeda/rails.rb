0879750dbe9a640b603e69c713611729c261958b
dks17 <kostya1917@gmail.com>
2022-06-06 10:04:51 +0300

    status code for exception

--- lib/yabeda/rails.rb
+++ lib/yabeda/rails.rb
@@ -58,7 +58,7 @@ module Yabeda
             labels = {
               controller: event.payload[:params]["controller"],
               action: event.payload[:params]["action"],
-              status: event.payload[:status],
+              status: Yabeda::Rails.event_status_code(event),
               format: event.payload[:format],
               method: event.payload[:method].downcase,
             }
@@ -79,6 +79,14 @@ module Yabeda
 
       def ms2s(milliseconds)
         (milliseconds.to_f / 1000).round(3)
+      end
+
+      def event_status_code(event)
+        if event.payload[:status].nil? && event.payload[:exception].present?
+          ActionDispatch::ExceptionWrapper.status_code_for_exception(event.payload[:exception].first)
+        else
+          event.payload[:status]
+        end
       end
     end
   end

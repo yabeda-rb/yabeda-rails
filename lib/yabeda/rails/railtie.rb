ff1a6037e9f02dc1e5c68e2ef542575c1d7623dc
Musaed Albrikan <32995191+mabrikan@users.noreply.github.com>
2025-09-09 07:32:18 +0000

    Add Passenger server to auto register list (#28)

--- lib/yabeda/rails/railtie.rb
+++ lib/yabeda/rails/railtie.rb
@@ -19,8 +19,12 @@ module Yabeda
         ::Rails.const_defined?("Unicorn::Launcher")
       end
 
+      def passenger_server?
+        ::Rails.const_defined?("PhusionPassenger")
+      end
+
       initializer "yabeda-rails.metrics" do
-        ::Yabeda::Rails.install! if rails_server? || puma_server? || unicorn_server?
+        ::Yabeda::Rails.install! if rails_server? || puma_server? || unicorn_server? || passenger_server?
       end
     end
   end
